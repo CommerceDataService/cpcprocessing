9cd867c2cada3f85e66f365a7a95b883d4c5b34e
Sasan Bahadaran <sbahadaran@doc.gov>
2017-03-31 09:30:29 -0400

    removed commented code and print statements from script

--- processcpcfiles.py
+++ processcpcfiles.py
@@ -1,3 +1,13 @@
+#!/usr/bin/python3.5
+
+#Author:        Sasan Bahadaran
+#Date:          3/31/17
+#Organization:  Commerce Data Service
+#Description:   This script is meant to parse files from
+# http://www.cooperativepatentclassification.org/cpcSchemeAndDefinitions/Bulk.html
+# in order to output cpc codes and related information into a structured csv file.
+
+
 import sys, os, csv
 from lxml import etree
 
@@ -10,31 +20,22 @@ def parseXML(filetoproc):
         root = tree.getroot()
         for item in root.xpath('//definitions/definition-item'):
             cpc_content = []
-            #print('---------------------')
             main_cpc_code = item.find('classification-symbol').text
             cpc_content.append(main_cpc_code)
-            #print('[main cpc code]: {}'.format(main_cpc_code))
             title_text = ''.join(item.xpath('definition-title/descendant-or-self::*/text()'))
             cpc_content.append(title_text)
-            #print('[title text]: {}'.format(title_text))
             title_text_codes = '|'.join(item.xpath('definition-title//class-ref/text()'))
             cpc_content.append(title_text_codes)
-            #print('[title cpc codes]: {}'.format(title_text_codes))
             informative_ref_text = ''.join(item.xpath('references/informative-references/section-body/table/table-row/table-column//paragraph-text/text()'))
             cpc_content.append(informative_ref_text)
-            #print('[informative references text]: {}'.format(informative_ref_text))
             informative_ref_cpc_codes = '|'.join(item.xpath('references/informative-references/section-body/table/table-row/table-column//paragraph-text/class-ref/text()'))
             cpc_content.append(informative_ref_cpc_codes)
-            #print('[informative references codes]: {}'.format(informative_ref_cpc_codes))
             special_rules_text = ''.join(item.xpath('special-rules/section-body//paragraph-text/text()'))
             cpc_content.append(special_rules_text)
-            #print('[special rules text]: {}'.format(special_rules_text))
             special_rules_cpc_codes = '|'.join(item.xpath('special-rules/section-body//paragraph-text/class-ref/text()'))
             cpc_content.append(special_rules_cpc_codes)
-            #print('[special rules codes]: {}'.format(special_rules_cpc_codes))
             glossary_of_terms = ''.join(item.xpath('glossary-of-terms/section-body/table/table-row/table-column//paragraph-text/text()'))
             cpc_content.append(glossary_of_terms)
-            #print('[glossary of terms]: {}'.format(glossary_of_terms))
             doccontent.append(cpc_content)
         return doccontent 
     except IOError as e:
@@ -47,8 +48,9 @@ def writeResults(content, fname):
         else:
             with open(fname,'w') as outfile:
                 writer = csv.writer(outfile, quoting=csv.QUOTE_ALL)
-                writer.writerow(('cpc_code', 'title', 'title_cpc_codes', 'informative_references', 'informative_references_cpc_codes', 'special_rules', 'special_rules_cpc_codes',\
-                    'glossary_of_terms'))
+                writer.writerow(('cpc_code', 'title', 'title_cpc_codes', 'informative_references', \
+                        'informative_references_cpc_codes', 'special_rules', 'special_rules_cpc_codes',\
+                        'glossary_of_terms'))
                 writer.writerows(content)
     except IOError as e:
         print('I/O error({0}): {1}'.format(e.errno,e.strerror))
@@ -70,20 +72,3 @@ if __name__ == '__main__':
                 output_fname = os.path.join(scriptpath, 'xml_files', fdir, (os.path.basename(fname)).split('-')[2]) 
                 output_fname = changeExt(output_fname, 'csv')
                 writeResults(content, output_fname)
-
-
-    #for root, dirs, files in os.walk(os.path.join(scriptpath, 'xml_files')):
-        #print(dirs)
-        #for fdir in dirs:
-            #for file in os.listdir(fdir):
-                #print(os.path.join(fdir, file))
-            #fname = 'cpc-definition-A01B.xml'
-            #filetoproc = os.path.join(scriptpath, fname)
-            
-            #content = parseXML(filetoproc)
-            #output_fname = os.path.join(scriptpath, (os.path.basename(fname)).split('-')[2]) 
-            #output_fname = changeExt(output_fname, 'csv')
-            #writeResults(content, output_fname)
-
-        
-
